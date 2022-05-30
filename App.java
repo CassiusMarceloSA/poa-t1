2a4fe5e431c6ca025a7ef43a6dcea834974f98f1
Cassius de Ávila <51337530+CassiusMarceloSA@users.noreply.github.com>
2022-05-30 20:01:56 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,18 +1,14 @@
 public class App {
     public static void main(String[] args) {
         String pat = "BAA";
-        String txt = "AACBACACBAA";
+        String txt = "AACBAZaBAAASJDIOSAJDOISAJDOIASDJSOAIDJSAIDJIAS";
         IterationCounter counter = new IterationCounter();
-        // KMP kmp = new KMP(counter);
-        // System.out.println("text: " + txt);
-        // int offset = kmp.search(pat, txt);
-        // System.out.print("pattern: ");
-        // for (int i = 0; i < offset; i++)
-        // System.out.print(" ");
-        // System.out.println(pat);
-        // System.out.println(counter.getCounter());
+        KMP kmp = new KMP(counter);
+        kmp.search(pat, txt);
+        System.out.println("Iterações [KMP]: " + counter.getCounter());
+        counter.reset();
         RabinKarp rk = new RabinKarp(counter);
-
-        System.out.println(rk.search(pat, txt));
+        rk.search(pat, txt);
+        System.out.println("Iterações [RK]: " + counter.getCounter());
     }
 }
